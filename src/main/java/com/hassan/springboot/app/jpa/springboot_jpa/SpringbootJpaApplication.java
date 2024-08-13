b4674734969e581f100e8facac56dfc3dfa38968
Hassan Abbud <hassanabbud@hotmail.com>
2024-08-13 17:50:09 -0600

    Get a single object with findById()

--- src/main/java/com/hassan/springboot/app/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/hassan/springboot/app/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,6 +1,7 @@
 package com.hassan.springboot.app.jpa.springboot_jpa;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -22,7 +23,13 @@ public class SpringbootJpaApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
+		// list();
+		findOne();
+
+	}
+
+	public void list(){
 		// List<Person> persons = (List<Person>) repository.findAll();
 		// By JPA keyword
 		// List<Person> persons = (List<Person>) repository.findByProgrammingLanguage("Java");
@@ -39,6 +46,20 @@ public class SpringbootJpaApplication implements CommandLineRunner{
 		personsValue.stream().forEach(person -> {
 			System.out.println(person[0] + " is an expert in " + person[1]);
 		});	
+	}
+
+	public void findOne(){
+		// Person person = null;
+		// Optional<Person> optionalPerson = repository.findById(5L);
+		// if(optionalPerson.isPresent()){
+		// 	person = optionalPerson.get();
+		// }
+		// System.out.println(person);
+		
+		// Person person = repository.findById(8L).orElse(null);
+		// System.out.println(person);
+
+		repository.findById(8L).ifPresent(p -> System.out.println(p));
 	}
 
 }
