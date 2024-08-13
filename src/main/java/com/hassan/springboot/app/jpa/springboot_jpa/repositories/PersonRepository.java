a76d73199ce66111f120392e6b6d35665998221f
Hassan Abbud <hassanabbud@hotmail.com>
2024-08-13 14:17:14 -0600

    Perform SQL query by method keywords using JPA

--- src/main/java/com/hassan/springboot/app/jpa/springboot_jpa/repositories/PersonRepository.java
+++ src/main/java/com/hassan/springboot/app/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -1,10 +1,14 @@
 package com.hassan.springboot.app.jpa.springboot_jpa.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.hassan.springboot.app.jpa.springboot_jpa.entities.Person;
 
 public interface PersonRepository extends CrudRepository<Person, Long>{
+    // Find via JPA method keywords (findby, Before, Or, And, etc.)
+    List<Person> findByProgrammingLanguage(String programmingLanguage);
+    List<Person> findByProgrammingLanguageAndName(String programmingLanguage, String name);
 
-    
 }
