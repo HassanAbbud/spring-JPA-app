c43944797d1a36bbbe00a56582555f81b6723fd0
Hassan Abbud <hassanabbud@hotmail.com>
2024-08-13 17:50:52 -0600

    Refactor imports

--- src/main/java/com/hassan/springboot/app/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/hassan/springboot/app/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,7 +1,6 @@
 package com.hassan.springboot.app.jpa.springboot_jpa;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
